af96fc00b04f010e7e818c48690b6217d670bc8a
Rajat Verma <cserajatverma@gmail.com>
2026-09-25 19:58:31 +0530

    Add resource validation middleware

---
+++ express-server/build/src/middleware/validateResource.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validateResource = void 0;
+var validateResource_1 = __importDefault(require("./validation/validateResource"));
+exports.validateResource = validateResource_1.default;
+exports.default = validateResource_1.default;
