6580f526dd64225ac4a10eee40767453165f482a
Rajat Verma <cserajatverma@gmail.com>
2026-09-25 19:58:30 +0530

    Add user deserialization middleware

---
+++ express-server/build/src/middleware/deserializeUser.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deserializeUser = void 0;
+var deserializeUser_1 = __importDefault(require("./auth/deserializeUser"));
+exports.deserializeUser = deserializeUser_1.default;
+exports.default = deserializeUser_1.default;
