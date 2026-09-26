3d97fccd32c42666e7d5b8e5e41a823d5a314479
Rajat Verma <cserajatverma@gmail.com>
2026-09-26 16:59:33 +0530

    Add chat controller

---
+++ express-server/build/src/modules/chat/chat.controller.js
@@ -0,0 +1,162 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.streamChatHandler = exports.postChatHandler = void 0;
+var chat_service_1 = require("./chat.service");
+var logger_1 = __importDefault(require("../../infrastructure/logger/logger"));
+function postChatHandler(req, res, next) {
+    return __awaiter(this, void 0, void 0, function () {
+        var userId, conversationId, content, result, err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    userId = res.locals.user._id;
+                    conversationId = req.params.conversationId;
+                    content = req.body.content;
+                    return [4 /*yield*/, (0, chat_service_1.processChatMessage)(userId, conversationId, content)];
+                case 1:
+                    result = _a.sent();
+                    return [2 /*return*/, res.status(201).json({
+                            success: true,
+                            data: result,
+                        })];
+                case 2:
+                    err_1 = _a.sent();
+                    return [2 /*return*/, next(err_1)];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.postChatHandler = postChatHandler;
+function streamChatHandler(req, res, next) {
+    return __awaiter(this, void 0, void 0, function () {
+        var userId, conversationId, content, headersSet, pingInterval, abortController, cleanup, sendEvent, result, err_2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    userId = res.locals.user._id;
+                    conversationId = req.params.conversationId;
+                    content = req.body.content;
+                    headersSet = false;
+                    pingInterval = null;
+                    abortController = new AbortController();
+                    cleanup = function () {
+                        if (pingInterval) {
+                            clearInterval(pingInterval);
+                            pingInterval = null;
+                        }
+                    };
+                    req.on("close", function () {
+                        if (!res.writableEnded) {
+                            abortController.abort();
+                        }
+                        cleanup();
+                    });
+                    sendEvent = function (event, data) {
+                        var payload = typeof data === "string" ? data : JSON.stringify(data);
+                        res.write("event: ".concat(event, "\ndata: ").concat(payload, "\n\n"));
+                    };
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, 4, 5]);
+                    return [4 /*yield*/, (0, chat_service_1.processStreamingChatMessage)(userId, conversationId, content, {
+                            signal: abortController.signal,
+                            onStart: function (data) {
+                                res.writeHead(200, {
+                                    "Content-Type": "text/event-stream",
+                                    "Cache-Control": "no-cache, no-transform",
+                                    Connection: "keep-alive",
+                                    "X-Accel-Buffering": "no",
+                                });
+                                headersSet = true;
+                                pingInterval = setInterval(function () {
+                                    if (!res.writableEnded) {
+                                        res.write(": ping\n\n");
+                                    }
+                                }, 15000);
+                                sendEvent("message.start", data);
+                            },
+                            onChunk: function (chunk) {
+                                if (chunk.type === "text_delta" && chunk.text) {
+                                    sendEvent("message.delta", { text: chunk.text });
+                                }
+                            },
+                        })];
+                case 2:
+                    result = _a.sent();
+                    if (!abortController.signal.aborted && headersSet) {
+                        sendEvent("message.completed", {
+                            userMessageId: result.userMessage._id,
+                            assistantMessageId: result.assistantMessage._id,
+                            usage: result.assistantMessage.tokenUsage,
+                            content: result.assistantMessage.content,
+                        });
+                        sendEvent("done", "[DONE]");
+                    }
+                    return [3 /*break*/, 5];
+                case 3:
+                    err_2 = _a.sent();
+                    cleanup();
+                    if (!headersSet && !res.headersSent) {
+                        return [2 /*return*/, next(err_2)];
+                    }
+                    logger_1.default.error({ err: err_2, conversationId: conversationId }, "Error during chat streaming");
+                    if (!res.writableEnded) {
+                        sendEvent("error", {
+                            message: err_2.message || "An unexpected error occurred during streaming",
+                            statusCode: err_2.statusCode || 500,
+                        });
+                    }
+                    return [3 /*break*/, 5];
+                case 4:
+                    cleanup();
+                    if (headersSet && !res.writableEnded) {
+                        res.end();
+                    }
+                    return [7 /*endfinally*/];
+                case 5: return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.streamChatHandler = streamChatHandler;
